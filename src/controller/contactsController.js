3ccf1aac46a0b1aa59d5c46ef8d020d85530eb81
ZOVUT YAKOV <fasiman991@gmail.com>
2026-07-07 09:09:58 +0300

    Update contactsController.js

--- src/controller/contactsController.js
+++ src/controller/contactsController.js
@@ -6,66 +6,51 @@ import {
   removeContact,
 } from "../services/contactsService.js";
 import HttpError from "../helpers/HttpError.js";
-import { contactSchema } from "../validators/contactValidator.js";
-import { deleteContactSchema } from "../validators/contactValidator.js";
-
+import { contactSchema, deleteContactSchema } from "../validators/contactValidator.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
-export function getContacts(req, res) {
+export const getContacts = ctrlWrapper((req, res) => {
   res.send(getAllContacts());
-}
+});
 
-export function getContactById(req, res) {
+export const getContactById = ctrlWrapper((req, res) => {
   const contact = getContactByIdOrNull(req.params.id);
 
   if (!contact) {
     return res.status(404).send({ error: "contact not found" });
   }
   res.send(contact);
-}
+});
 
-export function createContact(req, res, next) {
+export const createContact = ctrlWrapper((req, res, next) => {
   const { id, name, tel, country } = req.body;
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
+  const { error } = contactSchema.validate(req.body);
 
-    const newContact = addContact({ id, name, tel, country });
-    res.status(201).send(newContact);
-  } catch (error) {
-    next(error);
+  if (error) {
+    throw HttpError(400, error.message);
   }
-}
 
-export function updateContact(req, res) {
+  const newContact = addContact({ id, name, tel, country });
+  res.status(201).send(newContact);
+});
+
+export const updateContact = ctrlWrapper((req, res) => {
   const contact = patchContact(req.params.id, req.body);
   if (!contact) {
     return res.status(404).send({ error: "contact not found" });
   }
   res.send(contact);
-}
+});
 
-export function deleteContact(req, res, next) {
-  try {
-    const { error } = deleteContactSchema.validate(req.params);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-    const deleted = removeContact(req.params.id);
-    if (!deleted) {
-      return res.status(404).send({ error: "contact not found" });
-    }
-    res.send({ message: "contact deleted successfully" });
-  } catch (error) {
-    next(error);
+export const deleteContact = ctrlWrapper((req, res) => {
+  const { error } = deleteContactSchema.validate(req.params);
+  if (error) {
+    throw HttpError(400, error.message);
   }
-}
-export default {
-  getContacts: ctrlWrapper(getAllContacts),
-  getContactById: ctrlWrapper(getContactById),
-  createContact: ctrlWrapper(createContact),
-  deleteContact : ctrlWrapper(deleteContact)
-}
+
+  const deleted = removeContact(req.params.id);
+  if (!deleted) {
+    return res.status(404).send({ error: "contact not found" });
+  }
+  res.send({ message: "contact deleted successfully" });
+});
