5d9e63ba3844d73e02473e9400c1c78657ce1ed3
ZOVUT YAKOV <fasiman991@gmail.com>
2026-07-10 11:13:01 +0300

    Update contacts.js

--- db/contacts.js
+++ db/contacts.js
@@ -1,26 +1,3 @@
 export const contacts = [
-    {
-        "id": 121,
-        "name": "Yakov",
-        "tel": "8887123000",
-        "country": "US"
-    },
-    {
-        "id": 122,
-        "name": "Lev",
-        "tel": "677123000",
-        "country": "UA"
-    },
-    {
-        "id": 123,
-        "name": "Max",
-        "tel": "8887121323000",
-        "country": "DE"
-    },
-    {
-        "id": 124,
-        "name": "Fasiman",
-        "tel": "888752123000",
-        "country": "NL"
-    },
+  
 ];
